d6c25f66df22285b4a4a2c0bbacbd59a46411fe5
Faraz Shariff <faraz.shariff@enquero.com>
2021-08-10 18:19:32 +0530

    Intial gmail java code testing

---
+++ Gmail.java
@@ -0,0 +1,37 @@
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+
+	public static void main(String[] args)
+	{
+
+		System.out.println(" Welcome to Java Selenium ");
+		
+		System.setProperty("webdriver.chrome.driver","./driver2/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+	    driver.manage().window().maximize();
+
+	    String url = "https://accounts.google.com/signin";
+
+	    driver.get(url);
+
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+
+	    WebElement email_phone = driver.findElement(By.xpath("//input[@id='identifierId']"));
+
+	    email_phone.sendKeys("farazenquero");
+
+	    driver.findElement(By.id("identifierNext")).click();	
+
+	}
+
+}
